fbad9bb770ca3c4f206d0fcedf2e5527765f4cdf
Lee Min Hung <leminhhungtabletennis@gmail.com>
2022-06-26 00:58:17 +0700

    Refactor file structure

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,22 +1,231 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const Footer = () => {
+  const refBackToTop = useRef(null);
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= 500) {
+      refBackToTop.current.classList.add("active");
+    } else {
+      refBackToTop.current.classList.remove("active");
+    }
+  });
   return (
     <>
-      <div
-        className='text-center p-4 footer'
-        style={{ backgroundColor: "#fafafa" }}
-      >
-        © 2022 Copyright:{" "}
-        <a
-          className='text-reset fw-bold'
-          target='_blank'
-          rel='noreferrer'
-          href='https://github.com/emmannweb'
-        >
-          LBWF Electronic Ltd.
-        </a>
-      </div>
+      <footer className='footer'>
+        <div className='footer-top section'>
+          <div className='container'>
+            <div className='footer-brand'>
+              <a href='#' className='logo'>
+                <img
+                  src='/images/logo.svg'
+                  width='160'
+                  height='50'
+                  alt='Footcap logo'
+                />
+              </a>
+
+              <ul className='social-list'>
+                <li>
+                  <a href='#' className='social-link'>
+                    <ion-icon name='logo-facebook'></ion-icon>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='social-link'>
+                    <ion-icon name='logo-twitter'></ion-icon>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='social-link'>
+                    <ion-icon name='logo-pinterest'></ion-icon>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='social-link'>
+                    <ion-icon name='logo-linkedin'></ion-icon>
+                  </a>
+                </li>
+              </ul>
+            </div>
+
+            <div className='footer-link-box'>
+              <ul className='footer-list'>
+                <li>
+                  <p className='footer-list-title'>Contact Us</p>
+                </li>
+
+                <li>
+                  <address className='footer-link'>
+                    <ion-icon name='location'></ion-icon>
+
+                    <span className='footer-link-text'>
+                      Thon Den, Di Trach, Hoai Duc, Ha Noi, Viet Nam
+                    </span>
+                  </address>
+                </li>
+
+                <li>
+                  <a href='tel:+84843987789' className='footer-link'>
+                    <ion-icon name='call'></ion-icon>
+
+                    <span className='footer-link-text'>+84843987789</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a
+                    href='mailto:leminhhungtabletennis@gmail.com'
+                    className='footer-link'
+                  >
+                    <ion-icon name='mail'></ion-icon>
+
+                    <span className='footer-link-text'>
+                      leminhhungtabletennis@gmail.com
+                    </span>
+                  </a>
+                </li>
+              </ul>
+
+              <ul className='footer-list'>
+                <li>
+                  <p className='footer-list-title'>My Account</p>
+                </li>
+
+                <li>
+                  <a href='#' className='footer-link'>
+                    <ion-icon name='chevron-forward-outline'></ion-icon>
+
+                    <span className='footer-link-text'>My Account</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='footer-link'>
+                    <ion-icon name='chevron-forward-outline'></ion-icon>
+
+                    <span className='footer-link-text'>View Cart</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='footer-link'>
+                    <ion-icon name='chevron-forward-outline'></ion-icon>
+
+                    <span className='footer-link-text'>Wishlist</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='footer-link'>
+                    <ion-icon name='chevron-forward-outline'></ion-icon>
+
+                    <span className='footer-link-text'>Compare</span>
+                  </a>
+                </li>
+
+                <li>
+                  <a href='#' className='footer-link'>
+                    <ion-icon name='chevron-forward-outline'></ion-icon>
+
+                    <span className='footer-link-text'>New Products</span>
+                  </a>
+                </li>
+              </ul>
+
+              <div className='footer-list'>
+                <p className='footer-list-title'>Opening Time</p>
+
+                <table className='footer-table'>
+                  <tbody>
+                    <tr className='table-row'>
+                      <th className='table-head' scope='row'>
+                        Mon - Tue:
+                      </th>
+
+                      <td className='table-data'>8AM - 10PM</td>
+                    </tr>
+
+                    <tr className='table-row'>
+                      <th className='table-head' scope='row'>
+                        Wed:
+                      </th>
+
+                      <td className='table-data'>8AM - 7PM</td>
+                    </tr>
+
+                    <tr className='table-row'>
+                      <th className='table-head' scope='row'>
+                        Fri:
+                      </th>
+
+                      <td className='table-data'>7AM - 12PM</td>
+                    </tr>
+
+                    <tr className='table-row'>
+                      <th className='table-head' scope='row'>
+                        Sat:
+                      </th>
+
+                      <td className='table-data'>9AM - 8PM</td>
+                    </tr>
+
+                    <tr className='table-row'>
+                      <th className='table-head' scope='row'>
+                        Sun:
+                      </th>
+
+                      <td className='table-data'>Closed</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+
+              <div className='footer-list'>
+                <p className='footer-list-title'>Newsletter</p>
+
+                <p className='newsletter-text'>
+                  Authoritatively morph 24/7 potentialities with error-free
+                  partnerships.
+                </p>
+
+                <form action='' className='newsletter-form'>
+                  <input
+                    type='email'
+                    name='email'
+                    required
+                    placeholder='Email Address'
+                    className='newsletter-input'
+                  />
+
+                  <button type='submit' className='btn btn-primary'>
+                    Subscribe
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='footer-bottom'>
+          <div className='container'>
+            <p className='copyright'>
+              &copy; 2022{" "}
+              <a href='#' className='copyright-link'>
+                leminhhung
+              </a>
+              . All Rights Reserved
+            </p>
+          </div>
+        </div>
+      </footer>
+      {/* GO TO TOP */}
+      <a href='#top' class='go-top-btn' data-go-top ref={refBackToTop}>
+        <ion-icon name='arrow-up-outline'></ion-icon>
+      </a>
     </>
   );
 };
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,156 @@
-import React from "react";
-import mainImage from "../images/main.png";
+import React, { useRef } from "react";
 
-const Header = ({ title = "Title", description = "Description" }) => (
-  <img className='img-fluid' src={mainImage} alt={title} />
-);
+const Header = () => {
+  const refNavOpenBtn = useRef(null);
+  const refNavCloseBtn = useRef(null);
+  const refOverlay = useRef(null);
+  const refNavbar = useRef(null);
+  const refHeader = useRef(null);
+
+  window.addEventListener("scroll", function () {
+    if (window.scrollY >= 50) {
+      refHeader.current.classList.add("active");
+    } else {
+      refHeader.current.classList.remove("active");
+    }
+  });
+
+  const toggleNavbar = () => {
+    refNavbar.current.classList.toggle("active");
+    refOverlay.current.classList.toggle("active");
+  };
+
+  return (
+    <header className='header' data-header ref={refHeader}>
+      <div className='container'>
+        <div
+          className='overlay'
+          data-overlay
+          ref={refOverlay}
+          onClick={toggleNavbar}
+        ></div>
+        <a href='#' className='logo'>
+          <img
+            src='/images/logo.svg'
+            width='160'
+            height='45'
+            alt='Footcap logo'
+          />
+        </a>
+        <button
+          className='nav-open-btn'
+          data-nav-open-btn
+          aria-label='Open Menu'
+          ref={refNavOpenBtn}
+          onClick={toggleNavbar}
+        >
+          <ion-icon name='menu-outline'></ion-icon>
+        </button>
+        <nav className='navbar' data-navbar ref={refNavbar}>
+          <button
+            className='nav-close-btn'
+            data-nav-close-btn
+            aria-label='Close Menu'
+            ref={refNavCloseBtn}
+            onClick={toggleNavbar}
+          >
+            <ion-icon name='close-outline'></ion-icon>
+          </button>
+
+          <a href='#' className='logo'>
+            <img
+              src='/images/logo.svg'
+              width='190'
+              height='50'
+              alt='Footcap logo'
+            />
+          </a>
+
+          <ul className='navbar-list'>
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                Home
+              </a>
+            </li>
+
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                About
+              </a>
+            </li>
+
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                Products
+              </a>
+            </li>
+
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                Shop
+              </a>
+            </li>
+
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                Blog
+              </a>
+            </li>
+
+            <li className='navbar-item'>
+              <a href='#' className='navbar-link'>
+                Contact
+              </a>
+            </li>
+          </ul>
+
+          <ul className='nav-action-list'>
+            <li>
+              <button className='nav-action-btn'>
+                <ion-icon name='search-outline' aria-hidden='true'></ion-icon>
+
+                <span className='nav-action-text'>Search</span>
+              </button>
+            </li>
+
+            <li>
+              <a href='#' className='nav-action-btn'>
+                <ion-icon name='person-outline' aria-hidden='true'></ion-icon>
+
+                <span className='nav-action-text'>Login / Register</span>
+              </a>
+            </li>
+
+            <li>
+              <button className='nav-action-btn'>
+                <ion-icon name='heart-outline' aria-hidden='true'></ion-icon>
+
+                <span className='nav-action-text'>Wishlist</span>
+
+                <data className='nav-action-badge' value='5' aria-hidden='true'>
+                  5
+                </data>
+              </button>
+            </li>
+
+            <li>
+              <button className='nav-action-btn'>
+                <ion-icon name='bag-outline' aria-hidden='true'></ion-icon>
+
+                <data className='nav-action-text' value='318.00'>
+                  Basket: <strong>$318.00</strong>
+                </data>
+
+                <data className='nav-action-badge' value='4' aria-hidden='true'>
+                  4
+                </data>
+              </button>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
